22464efad3930ed0fa00b54d925b754de14f987e
hydronova <sahatsawat@todigi.net>
2025-08-17 15:29:39 +0700

    Get Flag

--- ph_sensor.py
+++ ph_sensor.py
@@ -7,6 +7,6 @@ client.connect("127.0.0.1", 1883)
 while True:
 	ph = round(random.uniform(6.5, 8.0), 2)
 	client.publish("sensor/ph", ph)
-	if ph > 10.0:
+	if ph > 10.0: #ph=12.5 for Flag
 		client.publish("system/status", "ALARM TRIGGERED")
 	time.sleep(1)
